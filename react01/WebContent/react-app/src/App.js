bec3497ed731eee316398631ba8c6e0761af8e95
coffeemori92 <coffeemori92@naver.com>
2019-06-13 10:03:10 +0900

    1

---
+++ react01/WebContent/react-app/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+	render(){
+		return (
+			<div className="App">
+		      Hello, React!!!
+		    </div>	
+		);
+	}
+}
+
+export default App;
+
+/*import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hello, React!!!
+    </div>
+  );
+}
+
+export default App;*/
